2fd4b600e2de5eb2bb7f8e149353d72146044b32
Arun <43026043+ceccs17d12@users.noreply.github.com>
2019-04-11 11:09:52 +0530

    Add files via upload

---
+++ Task8/perl/perlHello1.pl
@@ -0,0 +1,6 @@
+#!usr/bin/perl
+print "Heyy";
+$name=<STDIN>;
+print "Welcome To Perl!";
+
+
---
+++ Task8/perl/product.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+print "Enter two numbers \n";
+$a = <>;
+$b = <>;
+my $product = $a*$b;
+print "Product is = $product\n";
+
+
