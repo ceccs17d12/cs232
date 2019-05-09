79ffd8a85a458cd80c0c63c7d7824b4873e68ff9
Arun <43026043+ceccs17d12@users.noreply.github.com>
2019-05-09 16:05:04 +0530

    Create command.sh

---
+++ Task 1/command.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+ls
+ls -lk
+ls -1
+ls -lt
+ls -ltr
+alias ll='ls -l'
+ll
+unalias ll
+ll
+pwd
+cat > aa.txt
+cat aa.txt > bb.txt
+paste aa.txt bb.txt 
+grep "nikhil" aa.txt 
+cd Desktop/
+cp a.txt ../bb.txt .
+rm bb.txt 
+ls
+mkdir folder
+ls
+rmdir folder
+ls
+mkdir folder/folder2 -p
+ls folder/
+rm -r folder/
+ln -s ../bb.txt ./
+ls
+rm bb.txt 
+ls
+mv ../bb.txt ./
+ls
+cat > c.txt
+cat > d.txt
+join c.txt d.txt 
+exit
